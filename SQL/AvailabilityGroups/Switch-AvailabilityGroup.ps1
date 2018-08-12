ef7dc037f3eaaf1399ef8e3ab89d833aceaec254
Gerard Kelly <gakelly@github.com>
2018-08-12 13:25:00 +1000

    Removed extra lines

--- SQL/AvailabilityGroups/Switch-AvailabilityGroup.ps1
+++ SQL/AvailabilityGroups/Switch-AvailabilityGroup.ps1
@@ -1,84 +1,29 @@
  
-
-
 Function Switch-AvailabilityGroup {
-
-
     Param
-
-
     (
-
-
         [CmdletBinding()]
-
-
         # Param1 help description
-
-
-
         [Parameter(Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
-
-
         [String[]] $AGNames
-
-
     )
-
-
-
-　
 
 
 foreach ($AGName in $AGNames){
 
-
-
 Write-Output -Message "Looking for the Availability Group [$AGName]"
 
-
 $dbs = (Get-DbaAgReplica -SqlInstance $AGName -AvailabilityGroup $AGName | Select-Object role, replica -ErrorAction SilentlyContinue)
-
-
 $Instance = [PSCustomObject]@{
 
-
 $dbs[0].role = $dbs[0].replica
-
-
 $dbs[1].role = $dbs[1].replica
-
-
 }
-
-
 
 $Instance
-
-
-
 $ag = "SQLSERVER:\Sql\$($Instance.Secondary)\AvailabilityGroups\$AGName"
 
-
-
 Write-Output -Message "Switching Availability Group [$ag]"
-
-
-
 Switch-SqlAvailabilityGroup -Path $ag -Verbose
-
-
 }
-
-
 }
-
-
-
-　
-
-
-　
-
-
- 
